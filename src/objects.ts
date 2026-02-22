b2fb1d05747b3dc9110fe6a0ad5fcdc0ba0d5811
jacobdonn <jacobdonn21@gmail.com>
2026-02-22 11:50:44 -0500

    Finish isCorrect()

--- src/objects.ts
+++ src/objects.ts
@@ -30,7 +30,7 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return question.expected.toLowerCase() === answer.toLowerCase().trim();
 }
 
 /**
