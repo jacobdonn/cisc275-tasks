907117cbc541ace73573373c67713ebd9c2bd1d4
jacobdonn <jacobdonn21@gmail.com>
2026-02-22 17:14:28 -0500

    Finish isValid()

--- src/objects.ts
+++ src/objects.ts
@@ -41,7 +41,7 @@ export function isCorrect(question: Question, answer: string): boolean {
  */
 export function isValid(question: Question, answer: string): boolean {
     if (question.type === "multiple_choice_question") {
-        return question.expected === answer;
+        return question.options.includes(answer);
     }
 
     return answer != "";
