369a92c433114862d86fdb8b3ae25f1e3b1707ba
jacobdonn <jacobdonn21@gmail.com>
2026-03-03 17:26:56 -0500

    Finish changeType() component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-// import { ChangeType } from "./components/ChangeType";
+import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
@@ -27,9 +27,9 @@ function App(): React.JSX.Element {
             <hr />
             <TwoDice></TwoDice>
             <hr />
-            {/* <ChangeType></ChangeType>
+            <ChangeType></ChangeType>
             <hr />
-            <CycleHoliday></CycleHoliday> */}
+            {/* <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,23 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questType, setQuestType] = useState<QuestionType>(
+        "short_answer_question",
+    );
+
+    const changeType = (): void => {
+        setQuestType(
+            questType === "short_answer_question" ?
+                "multiple_choice_question"
+            :   "short_answer_question",
+        );
+    };
+
+    return (
+        <div>
+            <Button onClick={changeType}>Change Type</Button>
+            {questType === "short_answer_question" && "Short Answer"}
+            {questType === "multiple_choice_question" && "Multiple Choice"}
+        </div>
+    );
 }
