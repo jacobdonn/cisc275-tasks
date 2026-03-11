15c487bfa7722e122c6d4a04c4207654041a32d1
jacobdonn <jacobdonn21@gmail.com>
2026-03-10 21:42:25 -0400

    Pass all test from GiveAttempts() except recognizing "113"

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
-// import { GiveAttempts } from "./form-components/GiveAttempts";
+import { GiveAttempts } from "./form-components/GiveAttempts";
 // import { EditMode } from "./form-components/EditMode";
 // import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 // import { ChangeColor } from "./form-components/ChangeColor";
@@ -30,9 +30,9 @@ function App(): React.JSX.Element {
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
-            {/* <GiveAttempts></GiveAttempts>
+            <GiveAttempts></GiveAttempts>
             <hr></hr>
-            <EditMode></EditMode>
+            {/* <EditMode></EditMode>
             <hr></hr>
             <ChangeColor></ChangeColor>
             <hr></hr>
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attsLeft, setAttsLeft] = useState<number>(3);
+    const [attsRequested, setAttsRequested] = useState<string>("3");
+
+    function updateAttsRequested(event: React.ChangeEvent<HTMLInputElement>) {
+        setAttsRequested(event.target.value);
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group>
+                <Form.Label>Request more attempts</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attsRequested}
+                    onChange={updateAttsRequested}
+                />
+            </Form.Group>
+
+            <Button
+                onClick={() => {
+                    setAttsLeft(attsLeft - 1);
+                }}
+                disabled={attsLeft === 0}
+            >
+                use
+            </Button>
+
+            <Button
+                onClick={() => {
+                    setAttsLeft(attsLeft + +attsRequested);
+                }}
+            >
+                gain
+            </Button>
+            {attsLeft}
         </div>
     );
 }
